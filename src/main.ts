03780211ffe57c3623a1a40fd43f78a4382a1f6e
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-04-12 22:19:26 +0300

    imported fonts, Dashforge .css, created sign-up page

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
+
+// Import Dashforge CSS.
+import './assets/css/dashforge/dashforge.css'
+import './assets/css/dashforge/dashforge.auth.css'
+
+// Import Bootstrap 5 and Popper.js.
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TheHeaderVue from '@/components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeaderVue />
+  hello from sign up page.
+</template>
